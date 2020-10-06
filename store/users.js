a9d6208bc200aeb323759ab0030c3852371ca85e
Zach Ayers <ayers.dev@icloud.com>
2020-10-06 09:52:21 -0500

    adds git ignore

--- store/users.js
+++ store/users.js
@@ -1,12 +1,6 @@
 const users = [
   {
     id: 1,
-    name: "Mosh",
-    email: "mosh@domain.com",
-    password: "12345",
-  },
-  {
-    id: 2,
     name: "John",
     email: "john@domain.com",
     password: "12345",
